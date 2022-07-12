103a8adcf51d7a3a37ab028be9f07b47ce2344ae
Владислав Прусаков <v.prusakov@corp.mail.ru>
2022-07-12 15:58:55 +0300

    upd physics components

--- Sources/AdaEngine/Scene/Physics/Physics2D/Components/Collision2DComponent.swift
+++ Sources/AdaEngine/Scene/Physics/Physics2D/Components/Collision2DComponent.swift
@@ -9,16 +9,23 @@ public struct Collision2DComponent: Component {
     
     internal var runtimeBody: Body2D?
     public var shapes: [Shape2DResource] = []
-    public var mode: PhysicsBodyMode
+    public var mode: Mode
     public var filter: CollisionFilter
     
     public init(
         shapes: [Shape2DResource],
-        mode: PhysicsBodyMode = .static,
+        mode: Mode = .default,
         filter: CollisionFilter = CollisionFilter()
     ) {
         self.mode = mode
         self.shapes = shapes
         self.filter = filter
+    }
+}
+
+public extension Collision2DComponent {
+    enum Mode {
+        case trigger
+        case `default`
     }
 }
