0704bdc75d2ddbb584fdc6ffe18a6de3e71cc6df
Владислав Прусаков <v.prusakov@corp.mail.ru>
2022-08-11 15:04:24 +0300

    Update tuist settings, imporve physics 2d

---
+++ Sources/AdaEditor/Scenes/GameScene3D.swift
@@ -0,0 +1,9 @@
+//
+//  GameScene2D.swift
+//  AdaEditor
+//
+//  Created by v.prusakov on 8/11/22.
+//  Copyright © 2022 $(PRODUCT_BUNDLE_IDENTIFIER).editor. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/AdaEngine/Scene/Components/Physics2DWorldComponent.swift
@@ -0,0 +1,8 @@
+//
+//  Physics2DWorldComponent.swift
+//  AdaEngine
+//
+//  Created by v.prusakov on 8/11/22.
+//
+
+import Foundation
