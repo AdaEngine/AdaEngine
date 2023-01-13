9c5ef66aa9709dbb6544ad0d85b2e467a343f6f0
Владислав Прусаков <v.prusakov@corp.mail.ru>
2023-01-13 18:21:42 +0300

    Add Example for EmbeddableView

---
+++ Example/EmbeddableView/EmbeddableView/EmbeddableViewApp.swift
@@ -0,0 +1,19 @@
+//
+//  EmbeddableViewApp.swift
+//  EmbeddableView
+//
+//  Created by v.prusakov on 1/9/23.
+//
+
+import SwiftUI
+import AdaEngine
+
+@main
+struct EmbeddableViewApp: SwiftUI.App {
+    
+    var body: some SwiftUI.Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
