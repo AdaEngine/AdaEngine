83a5ac029995ed92d447a08a328864a2fe4569d7
Владислав Прусаков <v.prusakov@corp.mail.ru>
2023-03-17 14:42:33 +0300

    Update FVN Hash

--- Sources/AdaEngine/Utils/Hashes/UniqueHashable.swift
+++ Sources/AdaEngine/Utils/Hashes/UniqueHashable.swift
@@ -1,5 +1,5 @@
 //
-//  Hashes.swift
+//  UniqueHashable.swift
 //  
 //
 //  Created by v.prusakov on 3/17/23.
@@ -46,49 +46,4 @@ public protocol UniqueHasher {
     ///
     /// - Returns: The hash value calculated by the hasher.
     func finalize() -> Int
-}
-
-/// Calculate unique FNV Hash function
-@frozen public struct FNVHasher: UniqueHasher {
-    
-    @usableFromInline
-    var prime: Int = 16777619
-    
-    @usableFromInline
-    var hash: Int = 2166136261
-    
-    public init() { }
-    
-    @inlinable
-    public mutating func combine<H>(_ value: H) where H : UniqueHashable {
-        self.hash ^= value.uniqueHashValue
-        self.hash = hash & self.prime
-    }
-    
-    public mutating func combine(bytes: UnsafeRawBufferPointer) {
-        for index in 0..<bytes.count {
-            self.hash ^= Int(bytes[index])
-            self.hash = hash & self.prime
-        }
-    }
-    
-    public func finalize() -> Int {
-        Int(truncatingIfNeeded: self.hash)
-    }
-}
-
-extension String: UniqueHashable {
-    public func hash(into hasher: inout FNVHasher) {
-        self.utf8.withContiguousStorageIfAvailable { pointer in
-            hasher.combine(bytes: UnsafeRawBufferPointer(pointer))
-        }
-    }
-}
-
-extension Int: UniqueHashable {
-    public func hash(into hasher: inout FNVHasher) {
-        CollectionOfOne(self).withContiguousStorageIfAvailable { pointer in
-            hasher.combine(bytes: UnsafeRawBufferPointer(pointer))
-        }
-    }
 }
