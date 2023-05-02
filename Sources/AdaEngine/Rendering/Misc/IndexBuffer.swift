dc93757cfbe94f8f8dafd029c79e7ac330946e89
Владислав Прусаков <v.prusakov@corp.mail.ru>
2023-05-02 15:09:15 +0300

    Add more documentations

--- Sources/AdaEngine/Rendering/Misc/IndexBuffer.swift
+++ Sources/AdaEngine/Rendering/Misc/IndexBuffer.swift
@@ -5,13 +5,19 @@
 //  Created by v.prusakov on 1/18/23.
 //
 
-// TODO: (Vlad) Add documentations
-
+/// The index type for an index buffer that references vertices of geometric primitives.
 public enum IndexBufferFormat: UInt8 {
+    
+    /// A 32-bit unsigned integer used as a primitive index.
     case uInt32
+    
+    /// A 16-bit unsigned integer used as a primitive index.
     case uInt16
 }
 
+/// This protocol describe index buffer created for GPU usage.
 public protocol IndexBuffer: Buffer {
+    
+    /// Index type stored in the buffer.
     var indexFormat: IndexBufferFormat { get }
 }
--- Sources/AdaEngine/Scene/Resources/Texture/Texture.swift
+++ Sources/AdaEngine/Scene/Resources/Texture/Texture.swift
@@ -5,10 +5,14 @@
 //  Created by v.prusakov on 6/28/22.
 //
 
+/// Base class describing a texture.
 open class Texture: Resource, Codable {
     
     private(set) var gpuTexture: GPUTexture
+    
+    /// The sampler instance that describe how to render texture.
     public private(set) var sampler: Sampler
+    
     private(set) var textureType: TextureType
     
     public var resourcePath: String = ""
@@ -20,6 +24,7 @@ open class Texture: Resource, Codable {
         self.sampler = sampler
     }
     
+    /// Returns an ``Image`` instance.
     public var image: Image {
         if let image = RenderEngine.shared.getImage(from: self) {
             return image
@@ -50,19 +55,39 @@ open class Texture: Resource, Codable {
 }
 
 public extension Texture {
+    
+    /// The dimension of each image, including whether multiple images are arranged into an array or a cube.
     enum TextureType: UInt16, Codable {
+        
+        /// A one-dimensional texture image.
         case texture1D
+        
+        /// An array of one-dimensional texture images.
         case texture1DArray
+        
+        /// A two-dimensional texture image.
         case texture2D
+        
+        /// An array of two-dimensional texture images.
         case texture2DArray
+        
+        /// A two-dimensional texture image that uses more than one sample for each pixel.
         case texture2DMultisample
+        
+        /// An array of two-dimensional texture images that use more than one sample for each pixel.
         case texture2DMultisampleArray
         
+        /// A cube texture with six two-dimensional images.
         case textureCube
+        
+        /// A three-dimensional texture image.
         case texture3D
+        
+        /// A texture buffer.
         case textureBuffer
     }
     
+    /// An enumeration for the various options that determine how you can use a texture.
     struct Usage: OptionSet, Codable {
         
         public typealias RawValue = UInt8
@@ -73,8 +98,13 @@ public extension Texture {
             self.rawValue = rawValue
         }
         
+        /// An option for reading or sampling from the texture in a shader.
         public static var read = Usage(rawValue: 1 << 0)
+        
+        /// An option for writing to the texture in a shader.
         public static var write = Usage(rawValue: 1 << 1)
+        
+        /// An option for rendering to the texture in a render pass.
         public static var renderTarget = Usage(rawValue: 1 << 2)
     }
 }
