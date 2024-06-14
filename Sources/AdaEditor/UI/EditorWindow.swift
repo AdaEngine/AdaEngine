df0a86ca6fb89a2da317609b1b482e62e88e1263
Vladislav Prusakov <spectraldragon@bk.ru>
2024-06-14 10:29:56 +0300

    Fix color

--- Sources/AdaEditor/UI/EditorWindow.swift
+++ Sources/AdaEditor/UI/EditorWindow.swift
@@ -16,7 +16,7 @@ class EditorWindow: UIWindow {
         let blueView = UIView(frame: Rect(origin: Point(x: 0, y: 0), size: Size(width: 50, height: 50)))
         blueView.backgroundColor = .blue
 
-        let redView = UIView(frame: Rect(origin: Point(x: 100, y: 0), size: Size(width: 50, height: 50)))
+        let redView = UIView(frame: Rect(origin: Point(x: 100, y: 50), size: Size(width: 50, height: 50)))
 //        redView.zIndex = 1
         redView.backgroundColor = .red
 
@@ -27,9 +27,9 @@ class EditorWindow: UIWindow {
     override func draw(in rect: Rect, with context: GUIRenderContext) {
         print("Did render", rect)
         
-        var count = Int(rect.width / 50)
+        var count = Int(rect.width / 100)
         
-        var x: Float = 0
+        var x: Float = rect.minX
         
         for i in 0..<count {
             context.drawRect(.init(x: x, y: 50, width: 50, height: 50), color: Color.random())
