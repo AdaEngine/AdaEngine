b3219f118aef63669a734c2e57065afe9ef32281
Vladislav Prusakov <vladislav.prusakov@my.games>
2024-06-24 03:43:33 +0300

    Fix some bug

--- Sources/AdaEngine/UI/DSL/Canvas.swift
+++ Sources/AdaEngine/UI/DSL/Canvas.swift
@@ -5,19 +5,41 @@
 //  Created by Vladislav Prusakov on 21.06.2024.
 //
 
+import Math
+
 /// A view type that supports immediate mode drawing.
 public struct Canvas: Widget, WidgetNodeBuilder {
 
+    public typealias RenderBlock = (GUIRenderContext, Size) -> Void
+
     public typealias Body = Never
 
-    let drawBlock: (GUIRenderContext, Size) -> Void
+    let render: RenderBlock
 
-    public init(drawBlock: @escaping (GUIRenderContext, Size) -> Void) {
-        self.drawBlock = drawBlock
+    public init(render: @escaping RenderBlock) {
+        self.render = render
     }
 
     func makeWidgetNode(context: Context) -> WidgetNode {
-        return CanvasWidgetNode(content: self, drawBlock: self.drawBlock)
+        return CanvasWidgetNode(content: self, drawBlock: self.render)
+    }
+}
+
+@MainActor
+class CanvasWidgetNode: WidgetNode {
+
+    let drawBlock: Canvas.RenderBlock
+
+    init<Content: Widget>(content: Content, drawBlock: @escaping Canvas.RenderBlock) {
+        self.drawBlock = drawBlock
+        super.init(content: content)
     }
 
+    override func draw(with context: GUIRenderContext) {
+        context.translateBy(x: self.frame.origin.x, y: -self.frame.origin.y)
+
+        self.drawBlock(context, self.frame.size)
+
+        context.translateBy(x: -self.frame.origin.x, y: self.frame.origin.y)
+    }
 }
