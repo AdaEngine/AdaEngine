c5ac41e4fecd7395cd10c4e71ffbc53871cf1562
Vladislav Prusakov <spectraldragon@bk.ru>
2025-05-16 17:33:05 +0300

    upd

--- Modules/msdf-atlas-gen/AtlasFontGenerator/AtlasFontGenerator.cpp
+++ Modules/msdf-atlas-gen/AtlasFontGenerator/AtlasFontGenerator.cpp
@@ -51,10 +51,11 @@ AtlasBitmap* GenerateAtlas(
     return result;
 }
 
-FontAtlasGenerator::FontAtlasGenerator(const char* filePath, const char* fontName, const font_atlas_descriptor& fontDescriptor) : m_FontData(new FontData()), m_fontDescriptor(fontDescriptor)
-{
-    FontHolder fontHandler;
-    bool success = fontHandler.loadFont(filePath);
+FontAtlasGenerator* font_atlas_generator_create(const char* fontPath,
+                                                const char* fontName,
+                                                const font_atlas_descriptor& fontDescriptor) {
+                                                    FontHolder fontHandler;
+    bool success = fontHandler.loadFont(fontPath);
     
     if (success) {
         assert("Can't load font");
@@ -71,7 +72,8 @@ FontAtlasGenerator::FontAtlasGenerator(const char* filePath, const char* fontNam
         0,
     };
     
-    m_FontData->fontGeometry = FontGeometry(&m_FontData->glyphs);
+    FontData* fontData = new FontData();
+    fontData->fontGeometry = FontGeometry(&fontData->glyphs);
     
     for (int range = 0; range < 8; range += 2) {
         for (uint32_t c = charsetRanges[range]; c <= charsetRanges[range + 1]; c++)
@@ -117,14 +119,21 @@ FontAtlasGenerator::FontAtlasGenerator(const char* filePath, const char* fontNam
             }
         }
     }
-
+    return nullptr;
 }
 
-AtlasBitmap* FontAtlasGenerator::generateAtlasBitmap() {
+font_handle_t* font_atlas_generator_get_font_data(FontAtlasGenerator* generator) {
+    return nullptr;
+}
+void font_handle_destroy(font_handle_t* fontHandle) {
+    return;
+}
+
+AtlasBitmap* font_atlas_generator_generate_bitmap(FontAtlasGenerator* generator) {
     GenerationConfig config;
-    config.width = m_AtlasInfo.width;
-    config.height = m_AtlasInfo.height;
-    config.threads = m_fontDescriptor.threads;
+    config.width = generator->m_AtlasInfo.width;
+    config.height = generator->m_AtlasInfo.height;
+    config.threads = generator->m_fontDescriptor.threads;
     config.attributes.config.overlapSupport = true;
     config.attributes.scanlinePass = true;
     
@@ -145,5 +154,100 @@ AtlasBitmap* FontAtlasGenerator::generateAtlasBitmap() {
     
     return nullptr;
 }
+
+// FontAtlasGenerator::FontAtlasGenerator(const char* filePath, const char* fontName, const font_atlas_descriptor& fontDescriptor) : m_FontData(new FontData()), m_fontDescriptor(fontDescriptor)
+// {
+//     FontHolder fontHandler;
+//     bool success = fontHandler.loadFont(filePath);
+    
+//     if (success) {
+//         assert("Can't load font");
+//     }
+    
+//     Charset charset;
+    
+//     // From ImGui
+//     static const uint32_t charsetRanges[] = {
+//         0x0020, 0x00FF, // Basic Latin + Latin Supplement
+//         0x0400, 0x052F, // Cyrillic + Cyrillic Supplement
+//         0x2DE0, 0x2DFF, // Cyrillic Extended-A
+//         0xA640, 0xA69F, // Cyrillic Extended-B
+//         0,
+//     };
+    
+//     m_FontData->fontGeometry = FontGeometry(&m_FontData->glyphs);
+    
+//     for (int range = 0; range < 8; range += 2) {
+//         for (uint32_t c = charsetRanges[range]; c <= charsetRanges[range + 1]; c++)
+//             charset.add(c);
+//     }
+    
+//     int loadedGlyphs = m_FontData->fontGeometry.loadCharset(fontHandler.getFont(), 1, charset);
+    
+//     if (loadedGlyphs < m_FontData->glyphs.size()) {
+//         assert("Can't load all glyphs");
+//     }
+    
+//     m_FontData->fontGeometry.setName(fontName);
+    
+//     TightAtlasPacker atlasPacker;
+//     atlasPacker.setDimensionsConstraint(TightAtlasPacker::DimensionsConstraint::MULTIPLE_OF_FOUR_SQUARE);
+//     atlasPacker.setPadding(fontDescriptor.atlasImageType == AFG_ImageType::AFG_IMAGE_TYPE_MSDF || fontDescriptor.atlasImageType == AFG_ImageType::AFG_IMAGE_TYPE_MTSDF ? 0 : -1);
+    
+//     atlasPacker.setScale(fontDescriptor.emFontScale);
+//     atlasPacker.setMinimumScale(12);
+//     atlasPacker.setPixelRange(fontDescriptor.atlasPixelRange);
+//     atlasPacker.setMiterLimit(fontDescriptor.miterLimit);
+    
+//     int result = atlasPacker.pack(m_FontData->glyphs.data(), (int)m_FontData->glyphs.size());
+    
+//     if (result != 0)
+//         return;
+    
+//     atlasPacker.getDimensions(m_AtlasInfo.width, m_AtlasInfo.height);
+    
+//     if (fontDescriptor.atlasImageType == AFG_ImageType::AFG_IMAGE_TYPE_MSDF || fontDescriptor.atlasImageType == AFG_ImageType::AFG_IMAGE_TYPE_MTSDF) {
+//         if (fontDescriptor.expensiveColoring) {
+//             Workload([&glyphs = m_FontData->glyphs, &fontDescriptor](int i, int threadNo) -> bool {
+//                 unsigned long long glyphSeed = (LCG_MULTIPLIER * (fontDescriptor.coloringSeed ^ i) + LCG_INCREMENT) * !!fontDescriptor.coloringSeed;
+//                 glyphs[i].edgeColoring(msdfgen::edgeColoringInkTrap, fontDescriptor.angleThreshold, glyphSeed);
+//                 return true;
+//             }, (int)m_FontData->glyphs.size());
+//         } else {
+//             unsigned long glyphSeed = fontDescriptor.coloringSeed;
+//             for (GlyphGeometry &glyph : m_FontData->glyphs) {
+//                 glyphSeed *= LCG_MULTIPLIER;
+//                 glyph.edgeColoring(msdfgen::edgeColoringInkTrap, fontDescriptor.angleThreshold, glyphSeed);
+//             }
+//         }
+//     }
+
+// }
+
+// AtlasBitmap* FontAtlasGenerator::generateAtlasBitmap() {
+//     GenerationConfig config;
+//     config.width = m_AtlasInfo.width;
+//     config.height = m_AtlasInfo.height;
+//     config.threads = m_fontDescriptor.threads;
+//     config.attributes.config.overlapSupport = true;
+//     config.attributes.scanlinePass = true;
+    
+//     switch (m_fontDescriptor.atlasImageType) {
+//         case AFG_ImageType::AFG_IMAGE_TYPE_HARD_MASK:
+//             break;
+//         case AFG_ImageType::AFG_IMAGE_TYPE_SOFT_MASK:
+//             break;
+//         case AFG_ImageType::AFG_IMAGE_TYPE_SDF:
+//             break;
+//         case AFG_ImageType::AFG_IMAGE_TYPE_PSDF:
+//             break;
+//         case AFG_ImageType::AFG_IMAGE_TYPE_MSDF:
+//             return GenerateAtlas<float, float, 3, msdfGenerator>(m_FontData->glyphs, m_FontData->fontGeometry, config);
+//         case AFG_ImageType::AFG_IMAGE_TYPE_MTSDF:
+//             return GenerateAtlas<float, float, 4, mtsdfGenerator>(m_FontData->glyphs, m_FontData->fontGeometry, config);
+//     }
+    
+//     return nullptr;
+// }
 
 }
--- Modules/msdf-atlas-gen/AtlasFontGenerator/AtlasFontGenerator.h
+++ Modules/msdf-atlas-gen/AtlasFontGenerator/AtlasFontGenerator.h
@@ -8,47 +8,60 @@
 #ifndef AtlasFontGenerator_h
 #define AtlasFontGenerator_h
 
-#include <msdfgen.h>
-#include <msdf_atlas_gen.h>
-#include <string>
 #include "atlas_font_gen.h"
 
-namespace ada {
+typedef struct AtlasInfo {
+    int width;
+    int height;
+} AtlasInfo;
 
-struct FontData {
-    msdf_atlas::FontGeometry fontGeometry;
-    std::vector<msdf_atlas::GlyphGeometry> glyphs;
-};
+typedef struct FontData {
+    // msdf_atlas::FontGeometry fontGeometry;
+    // std::vector<msdf_atlas::GlyphGeometry> glyphs;
+    AtlasInfo atlasInfo;
+    unsigned long glyphsCount;
+} FontData;
 
-/// Generate font atlas from font path and specific font description.
-class FontAtlasGenerator {
-public:
-    FontAtlasGenerator(const char* fontPath,
-                       const char* fontName,
-                       const font_atlas_descriptor& fontDescriptor);
-    
-    
-    /// Returns bitmap representation.
-    AtlasBitmap* generateAtlasBitmap();
-    
-    
-    /// For some reasons we should store font data on Swift side.
-    /// And you must delete memory when FontData isn't used anymore!
-    FontData* getFontData() {
-        return m_FontData;
-    }
-    
-private:    
-    FontData* m_FontData;
-    
-    font_atlas_descriptor m_fontDescriptor;
-    
-    struct {
-        int width = -1;
-        int height = -1;
-    } m_AtlasInfo;
-};
+typedef struct FontAtlasGenerator {
+    FontData* fontData;
+    void *generator;
+} FontAtlasGenerator;
 
-}
+FontAtlasGenerator* font_atlas_generator_create(const char* fontPath,
+                                                const char* fontName,
+                                                const font_atlas_descriptor& fontDescriptor);
+
+font_handle_t* font_atlas_generator_get_font_data(FontAtlasGenerator* generator);
+void font_handle_destroy(font_handle_t* fontHandle);
+AtlasBitmap* font_atlas_generator_generate_bitmap(FontAtlasGenerator* generator);
+
+
+// /// Generate font atlas from font path and specific font description.
+// class FontAtlasGenerator {
+// public:
+//     FontAtlasGenerator(const char* fontPath,
+//                        const char* fontName,
+//                        const font_atlas_descriptor& fontDescriptor);
+    
+    
+//     /// Returns bitmap representation.
+//     AtlasBitmap* generateAtlasBitmap();
+    
+    
+//     /// For some reasons we should store font data on Swift side.
+//     /// And you must delete memory when FontData isn't used anymore!
+//     FontData* getFontData() {
+//         return m_FontData;
+//     }
+    
+// private:    
+//     FontData* m_FontData;
+    
+//     font_atlas_descriptor m_fontDescriptor;
+    
+//     struct {
+//         int width = -1;
+//         int height = -1;
+//     } m_AtlasInfo;
+// };
 #endif /* AtlasFontGenerator_h */
-
