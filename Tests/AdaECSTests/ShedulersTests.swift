0949ffe9be80f690ecccfe9285373c3edd66f3cc
Vladislav Prusakov <spectraldragon@bk.ru>
2025-11-13 01:24:51 +0300

    I broke something

--- Tests/AdaECSTests/ShedulersTests.swift
+++ Tests/AdaECSTests/ShedulersTests.swift
@@ -11,6 +11,19 @@ import Testing
 @Suite
 struct SchedulersTests {
     @Test
+    func `add system to existing schedule`() async throws {
+        let world = World()
+        world.addScheduler(.init(name: .test))
+        world.insertResource(CheckSystemMarker(value: 0))
+        world.addSystem(checkSystem.self, on: .test)
+
+        await world.runScheduler(.test)
+
+        let marker = try #require(world.getResource(CheckSystemMarker.self))
+        #expect(marker.value == 1, "CheckSystem should exists for Tests scheduler")
+    }
+
+    @Test
     func `add system to non existing schedule`() async throws {
         let world = World()
         
