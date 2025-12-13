cfe7ca9550733cdf549ea9a95f26b68a1db84c42
Vladislav Prusakov <spectraldragon@bk.ru>
2025-12-14 01:09:05 +0300

    Move msdf to Sources

--- Package.swift
+++ Package.swift
@@ -420,7 +420,7 @@ targets += [
     .adaTarget(
         name: "AtlasFontGenerator",
         dependencies: [
-            .product(name: "MSDFAtlasGen", package: "msdf-atlas-gen")
+            "MSDFAtlasGen"
         ],
         publicHeadersPath: "include"
     ),
@@ -589,90 +589,92 @@ targets += [
 //            .define("SPIRV_CROSS_C_API_REFLECT", to: "1")
 //        ]
 //    ),
-//    /// MSDF
-//    .target(
-//        name: "MSDFGen",
-//        dependencies: [
-//            "freetype",
-//            "tinyxml"
-//        ],
-//        path: "msdfgen",
-//        publicHeadersPath: ".",
-//        cxxSettings: [
-//            .define("MSDFGEN_USE_CPP11"),
-//            .headerSearchPath("..")
-//        ]
-//    ),
-//    .target(
-//        name: "MSDFAtlasGen",
-//        dependencies: [
-//            "MSDFGen"
-//        ],
-//        path: "msdf-atlas-gen",
-//        publicHeadersPath: ".",
-//        cxxSettings: [
-//            .define("_CRT_SECURE_NO_WARNINGS"),
-//            .headerSearchPath("..")
-//        ]
-//    ),
-//    .target(
-//        name: "freetype",
-//        path: "freetype",
-//        sources: [
-//            "src/autofit/autofit.c",
-//            "src/base/ftbase.c",
-//            "src/base/ftbbox.c",
-//            "src/base/ftbdf.c",
-//            "src/base/ftbitmap.c",
-//            "src/base/ftcid.c",
-//            "src/base/ftdebug.c",
-//            "src/base/ftfstype.c",
-//            "src/base/ftgasp.c",
-//            "src/base/ftglyph.c",
-//            "src/base/ftgxval.c",
-//            "src/base/ftinit.c",
-//            "src/base/ftmm.c",
-//            "src/base/ftotval.c",
-//            "src/base/ftpatent.c",
-//            "src/base/ftpfr.c",
-//            "src/base/ftstroke.c",
-//            "src/base/ftsynth.c",
-//            "src/base/ftsystem.c",
-//            "src/base/fttype1.c",
-//            "src/base/ftwinfnt.c",
-//            "src/bdf/bdf.c",
-//            "src/bzip2/ftbzip2.c",
-//            "src/cache/ftcache.c",
-//            "src/cff/cff.c",
-//            "src/cid/type1cid.c",
-//            "src/gzip/ftgzip.c",
-//            "src/lzw/ftlzw.c",
-//            "src/pcf/pcf.c",
-//            "src/pfr/pfr.c",
-//            "src/psaux/psaux.c",
-//            "src/pshinter/pshinter.c",
-//            "src/psnames/psnames.c",
-//            "src/raster/raster.c",
-//            "src/sdf/sdf.c",
-//            "src/sfnt/sfnt.c",
-//            "src/smooth/smooth.c",
-//            "src/truetype/truetype.c",
-//            "src/type1/type1.c",
-//            "src/type42/type42.c",
-//            "src/winfonts/winfnt.c"
-//        ],
-//        publicHeadersPath: "include",
-//        cSettings: [
-//         .define("FT2_BUILD_LIBRARY"),
-//         .define("_CRT_SECURE_NO_WARNINGS"),
-//         .define("_CRT_NONSTDC_NO_WARNINGS")
-//     ]
-// ),
-// .target(
-//     name: "tinyxml",
-//     path: "tinyxml",
-//         publicHeadersPath: "."
-//     )
+
+    // MSDF
+
+        .target(
+            name: "MSDFGen",
+            dependencies: [
+                "freetype",
+                "tinyxml"
+            ],
+            path: "Sources/msdf-atlas-gen/msdfgen",
+            publicHeadersPath: ".",
+            cxxSettings: [
+                .define("MSDFGEN_USE_CPP11"),
+                .headerSearchPath("..")
+            ]
+        ),
+    .target(
+        name: "MSDFAtlasGen",
+        dependencies: [
+            "MSDFGen"
+        ],
+        path: "Sources/msdf-atlas-gen/msdf-atlas-gen",
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .define("_CRT_SECURE_NO_WARNINGS"),
+            .headerSearchPath("..")
+        ]
+    ),
+    .target(
+        name: "freetype",
+        path: "Sources/msdf-atlas-gen/freetype",
+        sources: [
+            "src/autofit/autofit.c",
+            "src/base/ftbase.c",
+            "src/base/ftbbox.c",
+            "src/base/ftbdf.c",
+            "src/base/ftbitmap.c",
+            "src/base/ftcid.c",
+            "src/base/ftdebug.c",
+            "src/base/ftfstype.c",
+            "src/base/ftgasp.c",
+            "src/base/ftglyph.c",
+            "src/base/ftgxval.c",
+            "src/base/ftinit.c",
+            "src/base/ftmm.c",
+            "src/base/ftotval.c",
+            "src/base/ftpatent.c",
+            "src/base/ftpfr.c",
+            "src/base/ftstroke.c",
+            "src/base/ftsynth.c",
+            "src/base/ftsystem.c",
+            "src/base/fttype1.c",
+            "src/base/ftwinfnt.c",
+            "src/bdf/bdf.c",
+            "src/bzip2/ftbzip2.c",
+            "src/cache/ftcache.c",
+            "src/cff/cff.c",
+            "src/cid/type1cid.c",
+            "src/gzip/ftgzip.c",
+            "src/lzw/ftlzw.c",
+            "src/pcf/pcf.c",
+            "src/pfr/pfr.c",
+            "src/psaux/psaux.c",
+            "src/pshinter/pshinter.c",
+            "src/psnames/psnames.c",
+            "src/raster/raster.c",
+            "src/sdf/sdf.c",
+            "src/sfnt/sfnt.c",
+            "src/smooth/smooth.c",
+            "src/truetype/truetype.c",
+            "src/type1/type1.c",
+            "src/type42/type42.c",
+            "src/winfonts/winfnt.c"
+        ],
+        publicHeadersPath: "include",
+        cSettings: [
+            .define("FT2_BUILD_LIBRARY"),
+            .define("_CRT_SECURE_NO_WARNINGS"),
+            .define("_CRT_NONSTDC_NO_WARNINGS")
+        ]
+    ),
+    .target(
+        name: "tinyxml",
+        path: "Sources/msdf-atlas-gen/tinyxml",
+        publicHeadersPath: "."
+    )
 ]
 
 // MARK: - Tests
@@ -802,7 +804,6 @@ package.dependencies += [
     .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.1"),
     .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.62.1"),
 
-    .package(path: "Modules/msdf-atlas-gen"),
     .package(path: "Modules/SPIRV-Cross"),
     .package(path: "Modules/glslang"),
 ]
