87c87af98e0c3fbb13eb4a27117474e5af21a4d5
Vladislav Prusakov <spectraldragon@bk.ru>
2025-12-15 14:03:04 +0300

    Upd tutorial

--- Sources/AdaEngine/AdaEngine.docc/Resources/PlayerAndText/Code/PAT-1-6-makeScene.swift
+++ Sources/AdaEngine/AdaEngine.docc/Resources/PlayerAndText/Code/PAT-1-6-makeScene.swift
@@ -1,7 +1,17 @@
 import AdaEngine
 
-class PlayerAndTextScene: Scene {
-    override func sceneDidMove(to view: SceneView) {
-        try self.makePlayer(for: self)
+struct PlayerAndTextPlugin: Plugin {
+    let characterAtlas: TextureAtlas
+
+    func setup(in app: borrowing AppWorlds) {
+        self.makePlayer(in: app.main)
+    }
+
+    private func makePlayer(in world: World) {
+        world.spawn {
+            PlayerComponent()
+            Transform(position: [0, -300, 0])
+            Sprite(texture: characterAtlas[7, 1], size: Size(width: 24, height: 24))
+        }
     }
 }
