2a12b1157e46f99f8cab23d7f23d83d9ee4d6247
Vladislav Prusakov <spectraldragon@bk.ru>
2025-12-19 23:11:03 +0300

    some fixes

--- Sources/AdaText/TextBundle.swift
+++ Sources/AdaText/TextBundle.swift
@@ -7,20 +7,23 @@
 
 import AdaECS
 import AdaRender
+import AdaTransform
 
 @Bundle
 public struct Text2D {
     public var textComponent = TextComponent(text: AttributedText(""))
-    public let noFrustumCulling: NoFrustumCulling
+    public var transform: Transform
     public var visibility: Visibility
-
+    public let noFrustumCulling: NoFrustumCulling
 
     public init(
         textComponent: TextComponent = TextComponent(text: AttributedText("")),
+        transform: Transform = Transform(),
         noFrustumCulling: NoFrustumCulling = NoFrustumCulling(),
         visibility: Visibility = .visible
     ) {
         self.textComponent = textComponent
+        self.transform = transform
         self.visibility = visibility
         self.noFrustumCulling = noFrustumCulling
     }
